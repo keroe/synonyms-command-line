aaecd30c39aadf2ffa19d45a38333c3eaccf32e3
Kevin Rösch <kevin-roesch@gmx.net>
2020-03-04 11:24:12 +0100

    first commit

--- synonym.py
+++ synonym.py
@@ -20,38 +20,15 @@ res = requests.get(url)
 res.raise_for_status()
 html = bs4.BeautifulSoup(res.text, "html.parser")
 
-#print(html)
-
 all_synonyms = html.find_all("div", {"class": "upper-synonyms"})
-first_meaning = html.find_all("div", {"class": "synonyms-list-group first"})
 all_meaning = html.find_all("div", {"class": "synonyms-list-group"})
 
-# main meaning
-print('\033[94m' + first_meaning[0].text.strip() + '\033[0m')
-first_synonyms = all_synonyms[0]
-list_of_words = first_synonyms.text.split("\n")
-for word in list_of_words[1:5]:
-    sys.stdout.write(word + " : ")
-print("\n \n")
-
-try:
-  ans = raw_input('\033[93m More synonyms? (Y/n) \033[0m << ')[0].lower()
-except:
-  ans = ''
-if ans in ['yes', 'y', '']:
-    more_synonym = True
-elif ans in ['no', 'n']:
-    more_synonym = False
-else:
-    more_synonym = False
-    print("\033[31minvalid input. Close script.")
-
-print("\n \n")
-
-meaning_index = 1
-word_index = 1
+meaning_index = 0
+word_index = 0
+more_synonym = True
 while(more_synonym):
     try:
+        # filter the ad
         if all_meaning[meaning_index].text.strip() == "Anzeige":
             meaning_index += 1
         print('\033[94m' + all_meaning[meaning_index].text.strip() + '\033[0m')
@@ -73,28 +50,3 @@ while(more_synonym):
     except IndexError:
         print("No more synonyms.")
         exit()
-#table_types = mensa_html.find_all("td", {"class": "mensatype"})
-
-#table_food = mensa_html.find_all("table", {"class": "easy-tab-dot"})
-
-#column = []
-
-# regex = (\d+\,\d{1,2})
-'''for i, l in enumerate(table_types[0:6]):
-    tables = []
-    tables.append(str(l.text))
-    for lines in table_food[i]:
-        for k, child in enumerate(lines.children):
-            if k == 0 or k == 2:
-                continue
-            rest = child.text.split('[', 1)[0]
-            tables.append(str(rest))
-    column.append(tables)
-
-for columns in column:
-    print columns[0]
-    print '----------'
-    for food in columns[1:]:
-        print food
-    print '\n'
-'''
